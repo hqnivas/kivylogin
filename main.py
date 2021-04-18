7a76ddc274869e87b9935788c187274bdf478673
hqnivas <69433608+hqnivas@users.noreply.github.com>
2021-04-18 18:21:17 -0400

    Screen to size

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from kivymd.app import MDApp
-from kivy.core.window import Window
+#from kivy.core.window import Window
 
 class LoginApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Indigo"
         return
 
-Window.fullscreen = True
+#Window.fullscreen = True
 LoginApp().run()
 
